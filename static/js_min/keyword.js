3a29398eefcdc08d93f6b4889db075cae8741008
pmmms1226 <pmmms1226@gmail.com>
2018-02-07 21:42:10 +0900

    d

--- static/js_min/keyword.js
+++ static/js_min/keyword.js
@@ -1,6 +1,7 @@
+<script type="text/javascript">
 $("#start_ajax").click(function(){
    var company = $("#Company").val();
-   $("#compTitle").text(company);
+  // $("#compTitle").text(company);
     $.ajax({
         type:"GET",
         url:"http://169.56.88.197:9090/discovery/realtime/"+company,
@@ -8,10 +9,26 @@ $("#start_ajax").click(function(){
         dataType : "json",
         success: function(xml){
             var result = xml;
-            alert(result);
+            for(var i in result.results){
+              //document.write("url:"+result.results[i].url+"<br>");
+              var kd;
+              for(key in result.results[i].highlight){
+                if(key == "enriched_title.keywords.text"  ){
+                  //document.write(result.results[i].highlight[key]+" ");
+                  kd += "#"+result.results[i].highlight[key];
+                }
+                else if(key == "enriched_text.keywords.text"){
+                  //document.write(result.results[i].highlight[key]+" ");
+                  kd += "#"+result.results[i].highlight[key];
+                }
+              }
+              document.getElementById("keyword_list_all").innerHTML += "<a href=result.results[i].url>"+ kd;
+            //  document.write("<br>");
+            }
         },
         error: function(xhr, status, error) {
             alert(error);
         }
     });
 });
+</script>
