83498139fcb7f54b8914ecfbb5248c975e197032
pmmms1226 <pmmms1226@gmail.com>
2018-02-07 21:12:47 +0900

    asd

--- static/js_min/keyword.js
+++ static/js_min/keyword.js
@@ -2,7 +2,7 @@ $("#start_ajax").click(function(){
    var company = $("#Company").val();
    $("#compTitle").text(company);
     $.ajax({
-        type:"POST",
+        type:"GET",
         url:"http://169.56.88.197:9090/discovery/realtime/"+company,
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
         dataType : "json",
