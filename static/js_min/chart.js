9ae6180ff2b7e0e997be5eebbd811e2da5d58d2b
pmmms1226 <pmmms1226@gmail.com>
2018-02-07 22:18:34 +0900

    asd

--- static/js_min/chart.js
+++ static/js_min/chart.js
@@ -1,17 +1,15 @@
 
-<script src="{{ url_for('static',filename='js_min/keyword.js') }}"></script>
 $("#start_ajax").click(function(){
-   var company = $("#Company").val();
-   $("#compTitle").text(company);
     $.ajax({
         type:"POST",
         url:"http://169.56.88.197:8080/chart ",
         contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-        data: {name: company},
+        data: {name:"삼성전자"},
         dataType : "json",
         success: function(xml){
             var result = xml;
-            Plotly.plot(ids="Chart",result.data,result.layout || {});
+            Plotly.plot(ids="1",result.data,result.layout || {});
+            alert("hhello");
             keyword();
         },
         error: function(xhr, status, error) {
@@ -19,3 +17,37 @@ $("#start_ajax").click(function(){
         }
     });
 });
+function keyword(){
+//   var company = $("#Company").val();
+  // $("#compTitle").text(company);
+  alert("keyword");
+    $.ajax({
+        type:"GET",
+        url:"http://169.56.88.197:9090/discovery/realtime/"+"삼성전자",
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        dataType : "json",
+        success: function(xml){
+            var result = xml;
+            for(var i in result.results){
+              //document.write("url:"+result.results[i].url+"<br>");
+              var kd;
+              for(key in result.results[i].highlight){
+                if(key == "enriched_title.keywords.text"  ){
+                  //document.write(result.results[i].highlight[key]+" ");
+                  kd = "#"+result.results[i].highlight[key];
+                  document.getElementById("keyword_list_all").innerHTML += "<a href="+result.results[i].url+">"+ kd +"<br>";
+                }
+                // else if(key == "enriched_text.keywords.text"){
+                //   //document.write(result.results[i].highlight[key]+" ");
+                //   kd += "#"+result.results[i].highlight[key];
+                // }
+              }
+
+            //  document.write("<br>");
+            }
+        },
+        error: function(xhr, status, error) {
+            alert(error);
+        }
+    });
+}
