4f90cf8b4c4241478fe0997e06eb58ca2b943082
pmmms1226 <pmmms1226@gmail.com>
2018-02-07 17:08:54 +0900

    aa

---
+++ static/js_min/keyword.js
@@ -0,0 +1,17 @@
+$("#start_ajax").click(function(){
+   var company = $("#Company").val();
+   $("#compTitle").text(company);
+    $.ajax({
+        type:"POST",
+        url:"http://169.56.88.197:9090/discovery/realtime/"+company,
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        dataType : "json",
+        success: function(xml){
+            var result = xml;
+            alert(result);
+        },
+        error: function(xhr, status, error) {
+            alert(error);
+        }
+    });
+});
