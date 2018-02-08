53fed8cb7114a84671fd883419c316603ffae7b3
pmmms1226 <pmmms1226@gmail.com>
2018-02-08 15:29:29 +0900

    Signed-off-by: pmmms1226 <pmmms1226@gmail.com>

--- static/js_min/chart.js
+++ static/js_min/chart.js
@@ -38,7 +38,7 @@ $("#start_ajax2").click(function(){
               if(key == "enriched_title.keywords.text"  ){
                 //document.write(result.results[i].highlight[key]+" ");
                 kd = "#"+result.results[i].highlight[key];
-                document.getElementById("keyword_list_all2").innerHTML += "<a href="+result.results[i].url+"style=\"color:rgb(255,255,255)\">"+ kd+"&nbsp";
+                document.getElementById("keyword_list_all2").innerHTML += "<a href="+result.results[i].url+"style=\"color:rgb(255,255,255)\">"+ kd+"&nbsp;";
               }
               // else if(key == "enriched_text.keywords.text"){
               //   //document.write(result.results[i].highlight[key]+" ");
@@ -72,7 +72,7 @@ function keyword(){
                 if(key == "enriched_title.keywords.text"  ){
                   //document.write(result.results[i].highlight[key]+" ");
                   kd = "#"+result.results[i].highlight[key];
-                  document.getElementById("keyword_list_all").innerHTML += "<a href="+result.results[i].url+"style=color:rgb(0,0,0)>"+ kd+"&nbsp";
+                  document.getElementById("keyword_list_all").innerHTML += "<a href="+result.results[i].url+"style=color:rgb(0,0,0)>"+ kd+"&nbsp;";
                 }
                 // else if(key == "enriched_text.keywords.text"){
                 //   //document.write(result.results[i].highlight[key]+" ");
